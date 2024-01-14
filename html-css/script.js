8787d456c2845184a714cf305b43ff36bb6a0135
xfmayvala <156601541+xfmayvala@users.noreply.github.com>
2024-01-14 21:51:05 +0400

    Add files via upload

---
+++ html-css/script.js
@@ -0,0 +1,17 @@
+function openPage() {
+    window.open("file:///C:/Users/lizi/Desktop/html-css/another.html", "_self");
+   
+   }
+   function openPage1() {
+    window.open("file:///C:/Users/lizi/Desktop/html-css/another1.html", "_self");
+   
+   }
+
+   function openPage2() {
+    window.open("file:///C:/Users/lizi/Desktop/html-css/another2.html", "_self");
+   
+   }
+   function openPage3() {
+    window.open("", "_self");
+   
+   }
